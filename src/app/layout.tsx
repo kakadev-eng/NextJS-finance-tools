7245e0e10ee6a68f12f671d4ebda7d3f4c1ee5cb
fanderson kaecio valencio <kaecio@yahoo.com.br>
2026-05-19 19:52:37 -0300

    adde google validation search

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,36 +5,29 @@ import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 
 export const metadata: Metadata = {
+  verification: {
+    google: "ixh4gTcuthKwTSscSVEekkWmEyju3IgCzmy50dq6WxI",
+  },
   title: {
     default: "FinanceTools",
     template: "%s | FinanceTools",
   },
-
   description:
     "Financial calculators, stock prices, crypto tools and market insights.",
-
   metadataBase: new URL(
     "https://finance-tools.vercel.app"
   ),
-
   openGraph: {
     title: "FinanceTools",
-
     description:
       "Financial calculators and market tools.",
-
     siteName: "FinanceTools",
-
     locale: "en_US",
-
     type: "website",
   },
-
   twitter: {
     card: "summary_large_image",
-
     title: "FinanceTools",
-
     description:
       "Financial calculators and market tools.",
   },
