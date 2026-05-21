6ecc5bfa67a12b938fa6a32414e75ed6c1957e83
fanderson kaecio valencio <kaecio@yahoo.com.br>
2026-05-20 23:53:32 -0300

    added test google adsense

--- src/components/GoogleAdSense.tsx
+++ src/components/GoogleAdSense.tsx
@@ -1,17 +1,44 @@
+// "use client";
+
+// import Script from "next/script";
+
+// const ADSENSE_ID =
+//   process.env.NEXT_PUBLIC_ADSENSE_ID!;
+
+// export function GoogleAdSense() {
+//   return (
+//     <Script
+//       async
+//       strategy="afterInteractive"
+//       crossOrigin="anonymous"
+//       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADSENSE_ID}`}
+//     />
+//   );
+// }
 "use client";
 
-import Script from "next/script";
+import { useEffect } from "react";
 
 const ADSENSE_ID =
   process.env.NEXT_PUBLIC_ADSENSE_ID!;
 
 export function GoogleAdSense() {
-  return (
-    <Script
-      async
-      strategy="afterInteractive"
-      crossOrigin="anonymous"
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADSENSE_ID}`}
-    />
-  );
+  useEffect(() => {
+    const script =
+      document.createElement("script");
+
+    script.async = true;
+
+    script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADSENSE_ID}`;
+
+    script.crossOrigin = "anonymous";
+
+    document.head.appendChild(script);
+
+    console.log(
+      "ADSENSE SCRIPT ADDED"
+    );
+  }, []);
+
+  return null;
 }
